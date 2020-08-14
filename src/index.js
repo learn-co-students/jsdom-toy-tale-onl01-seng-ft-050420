37a68b22c53a351911cfe179981b01ec36cb725e
Steve Anderson <prototype909@msn.com>
2020-08-13 18:40:23 -0600

    added listerner and subform

--- src/index.js
+++ src/index.js
@@ -1,6 +1,86 @@
 let addToy = false;
+const toysurl = "http://localhost:3000/toys";
+// document.addEventListener("DOMContentLoaded", () => {
+//   fetchToys();
+// })
+const addLike = function(event) {
+  event.preventDefault()
+  let likedToy = document.getElementById(event.target.id);
+  let previousLikes = parseInt(likedToy.querySelector("span").innerText); // this isn't reading a number as in shouwing up as nill
+  let updatedLikes = previousLikes + 1; // why is this not a number
+  const likeConfig = {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      "likes": updatedLikes
+    })
+  };
+  let toyId = event.target.id;
+  let url = `http://localhost:3000/toys/${toyId}`; 
+  fetch(url, likeConfig)
+    .then(resp => resp.json())
+    .then((json) => {likedToy.querySelector('span').innerText = updatedLikes})
+}
+function renderToyDivCards(json) {
+  console.log(json);
+  const toyCollection = document.getElementById('toy-collection')
+  json.map((toy) => {
+    const toyCardDiv = document.createElement('div');
+    toyCardDiv.setAttribute('id', toy.id);
+    toyCardDiv.classList.add('card');
+    const toyName = document.createElement('h2');
+    toyName.textContent = toy.name;
+    toyCardDiv.appendChild(toyName);
+    const toyImage = document.createElement('img');
+    toyImage.setAttribute('src', toy.image);
+    toyImage.classList.add('toy-avatar');
+    toyCardDiv.appendChild(toyImage);
+    const toyLikes = document.createElement('p');
+    toyLikes.innerHTML = `<span>${toy.likes}</span> likes`; // integer conversion is not displaying
+    toyCardDiv.appendChild(toyLikes);
+    const likeButton = document.createElement('button');
+    likeButton.setAttribute('id', toy.id);
+    likeButton.classList.add('like-button');
+    likeButton.innerText = "like";
+    likeButton.addEventListener('click', addLike);
+    toyCardDiv.appendChild(likeButton);
+    toyCollection.appendChild(toyCardDiv)
+  }) 
+}
+function fetchToys() {
+  return fetch(toysurl)
+    .then((resp) => { 
+      return resp.json();
+    })
+    // .then((json) => {
+    //   return renderToyDivCards(json)
+    // }) 
+    .then(renderToyDivCards)
+}
+
+
+
+document.addEventListener("DOMContentLoaded", ()=>{
+  const addToyForm = document.querySelector('#addtoyform')
+  addToyForm.addEventListener('submit', function (event) {
+    submitData(event.name, event.image)
+  })
+})
+
+
+function submitData(name, image) {
+  let formData = {
+    name: name,
+    image: image,
+    likes: 0
+  };
+
 
 document.addEventListener("DOMContentLoaded", () => {
+  fetchToys()
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
   addBtn.addEventListener("click", () => {
@@ -13,3 +93,6 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+
+
