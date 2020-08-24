336600a1da05f1b932b447f3481b67451ff147cd
Steve Anderson <prototype909@msn.com>
2020-08-24 02:54:50 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,92 +1,110 @@
 let addToy = false;
-const toysurl = "http://localhost:3000/toys";
-// document.addEventListener("DOMContentLoaded", () => {
-//   fetchToys();
-// })
-const addLike = function(event) {
-  event.preventDefault()
-  debugger
-  let likedToy = document.getElementById(event.target.id);
-  let previousLikes = parseInt(likedToy.querySelector("span").innerText); // this isn't reading a number as in shouwing up as nill
-  let updatedLikes = previousLikes + 1; // why is this not a number
-  const likeConfig = {
-    method: "PATCH",
-    headers: {
-      "Content-Type": "application/json",
-      "Accept": "application/json"
-    },
-    body: JSON.stringify({
-      "likes": updatedLikes
-    })
-  };
-  let toyId = event.target.id;
-  let url = `http://localhost:3000/toys/${toyId}`; 
-  fetch(url, likeConfig)
-    .then(resp => resp.json())
-    .then((json) => {likedToy.querySelector('span').innerText = updatedLikes})
-}
-function renderToyDivCards(json) {
-  console.log(json);
-  const toyCollection = document.getElementById('toy-collection')
-  json.map((toy) => {
-    const toyCardDiv = document.createElement('div');
-    toyCardDiv.setAttribute('id', toy.id);
-    toyCardDiv.classList.add('card');
-    const toyName = document.createElement('h2');
-    toyName.textContent = toy.name;
-    toyCardDiv.appendChild(toyName);
-    const toyImage = document.createElement('img');
-    toyImage.setAttribute('src', toy.image);
-    toyImage.classList.add('toy-avatar');
-    toyCardDiv.appendChild(toyImage);
-    const toyLikes = document.createElement('p');
-    toyLikes.innerHTML = `<span>${toy.likes}</span> likes`; // integer conversion is not displaying
-    toyCardDiv.appendChild(toyLikes);
-    const likeButton = document.createElement('button');
-    likeButton.setAttribute('id', toy.id);
-    likeButton.classList.add('like-button');
-    likeButton.innerText = "like";
-    likeButton.addEventListener('click', addLike);
-    toyCardDiv.appendChild(likeButton);
-    toyCollection.appendChild(toyCardDiv)
-    const editButton = document.createElement('button');
-    editButton.setAttribute('id', `edit-${toy.id}`)
-    editButton.innerText = "edit";
-    toyCardDiv.appendChild(editButton);
-    editButton.addEventListener('click', clickEditButton);
-  }) 
-}
 
-function clickEditButton(event) {
-  let button = event.target
-    if (button.innerText === "edit") {
-      const h2 = event.target.parentElement.querySelector('h2')
-      const toyName = event.target.parentElement.querySelector('h2').innerText
-      button.innerText = "save"
-      h2.innerHTML = `<input type='text' value='${toyName}' id='toy-name'>`
-
-    } else {
-      //change button to say edit.
-      //grab the input value.
-      //change the innerHTML of our 'h2' to be the new input value (name).
-      //make a patch request to the backend to update the database and the DOM.
-    }
-  debugger
-}
-
-
-function fetchToys() {
-  return fetch(toysurl)
-    .then((resp) => { 
-      return resp.json();
-    })
-    // .then((json) => {
-    //   return renderToyDivCards(json)
-    // }) 
-    .then(renderToyDivCards)
-}
 document.addEventListener("DOMContentLoaded", () => {
-  fetchToys()
+
+  fetch('http://localhost:3000/toys')
+  .then(response => response.json())
+  .then(data => cardsInfo(data));
+
+  function createCard(toy) {
+    let card = document.createElement('div');
+    card.setAttribute('class', 'card');
+    let h2 = document.createElement("h2");
+    h2.textContent = toy.name
+    card.appendChild(h2)
+    let image = document.createElement("img");
+    image.setAttribute('src',toy.image);
+    image.setAttribute('class', 'toy-avatar');
+    card.appendChild(image);
+    let p = document.createElement("p");
+    p.textContent = `${toy.likes} likes`;
+    card.appendChild(p);
+    let button = document.createElement("button");
+    button.setAttribute('class','like-btn');
+    button.textContent = `Like <3`;
+    button.addEventListener('click', (event) => {
+      toy.likes = toy.likes + 1;
+      p.textContent = `${toy.likes} likes`;
+      increaseLikes(toy);
+    });
+    card.appendChild(button);
+    let editButton = document.createElement("button");
+    editButton.textContent = `Edit`;
+    editButton.addEventListener('click',(event) => {
+      if(editButton.innerText === "Save") {
+        editButton.innerText = "Edit"
+        //grab value of inpt field
+        let newToyName = document.getElementById(`edit-${toy.id}`).value
+        // make patch request to update toy
+        //remove input field
+
+        h2.innerHTML = newToyName
+      } else {
+        editButton.innerText = "Save"
+        h2.innerHTML= `<input type='text' id='edit-${toy.id}' value='${h2.innerText}' ></input>`
+        //replace the tpys name with input field
+      }
+    })
+    card.appendChild(editButton);
+    let toyCollection = document.getElementById("toy-collection");
+    toyCollection.appendChild(card);
+  }
+
+
+
+  function cardsInfo(toys){
+    for (const toy of toys){
+      createCard(toy);
+    }
+  }
+
+  function increaseLikes(toy) {
+      let formData = {
+      likes: toy.likes
+    };
+    let configObj = {
+      method: "PATCH",
+      headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+      },
+      body: JSON.stringify(formData)
+    };
+    return fetch(`http://localhost:3000/toys/${toy.id}`, configObj)
+  }
+
+  let addToyForm = document.getElementsByClassName("add-toy-form")[0];
+  addToyForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    let nameInput = document.getElementsByName("name")[0];
+    let imageInput = document.getElementsByName("image")[0];
+    submitData(nameInput.value, imageInput.value);
+    nameInput.value = "";
+    imageInput.value = "";
+  });
+
+  function submitData(name,image){
+    let formData = {
+      name: name,
+      image: image,
+      likes: 0
+    };
+    let configObj = {
+      method: "POST",
+      headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+      },
+      body: JSON.stringify(formData)
+    };
+    return fetch("http://localhost:3000/toys", configObj)
+    .then(resp => resp.json())
+    .then(toy => {
+      createCard(toy);
+
+    });
+  }
+
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
   addBtn.addEventListener("click", () => {
